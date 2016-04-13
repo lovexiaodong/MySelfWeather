8ace94981489629f7342ab57d42906277aa87f58
lovexiaodong <zydqlg@sina.com>
2016-04-13 23:12:01 +0800

    add Show weather layout xml

--- app/src/main/java/com/example/com/myselefweather/util/Utility.java
+++ app/src/main/java/com/example/com/myselefweather/util/Utility.java
@@ -1,11 +1,20 @@
 package com.example.com.myselefweather.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.example.com.myselefweather.db.WeatherDB;
 import com.example.com.myselefweather.model.City;
 import com.example.com.myselefweather.model.County;
 import com.example.com.myselefweather.model.Province;
+
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Administrator on 2016/4/12.
@@ -74,5 +83,38 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static void saveWeatherInfo(Context context, String cityName, String cityCode, String temp1,
+                                       String temp2, String weatherDesp, String publishTime)
+    {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.CHINA);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_selected", true);
+        editor.putString("city_name", cityName);
+        editor.putString("city_code", cityCode);
+        editor.putString("temp1", temp1);
+        editor.putString("temp2", temp2);
+        editor.putString("weatherDesp", weatherDesp);
+        editor.putString("publish", publishTime);
+        editor.putString("current_data",format.format(new Date()));
+        editor.commit();
+    }
+
+    public static void handleWeatherResponse(Context context, String response)
+    {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+            String cityName = weatherInfo.getString("city");
+            String cityCode = weatherInfo.getString("cityid");
+            String tem1 = weatherInfo.getString("temp1");
+            String tem2 = weatherInfo.getString("temp2");
+            String weatherDesp = weatherInfo.getString("weather");
+            String publishTime = weatherInfo.getString("ptime");
+            saveWeatherInfo(context,cityName, cityCode, tem1, tem2, weatherDesp, publishTime);
+
+        }catch (Exception e)
+        {}
     }
 }
